1d4aee37ccd99e5da4ed19416b7b079a85a1a06b
triggering-event <gabriel.j.shapiro@gmail.com>
2020-04-26 19:20:17 -0400

    completes Post component (adding Like state from props)

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -9,17 +9,17 @@ const LikeSection = props => {
   return (
     <div>
       <div
-        className="like-section"
-        key="likes-icons-container"
+          className="like-section"
+          key="likes-icons-container"
       >
-        <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
-        </div>
-        <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faComment} />
-        </div>
+          <div className="like-section-wrapper">
+              <FontAwesomeIcon icon={faHeart}/>
+          </div>
+          <div className="like-section-wrapper">
+              <FontAwesomeIcon icon={faComment}/>
+          </div>
       </div>
-      <p className="like-number">27 likes</p>
+        <p className="like-number">{props.likes} likes</p>
     </div>
   )
 };
