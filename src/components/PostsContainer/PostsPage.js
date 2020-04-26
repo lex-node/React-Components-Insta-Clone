61bf5095a1f3c529f49bbee6c182571e21a25553
triggering-event <gabriel.j.shapiro@gmail.com>
2020-04-26 19:11:17 -0400

    completes Posts component

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,17 +1,17 @@
-//Complete the necessary code in this file
-// import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+import dummyData from "../../dummy-data";
 
 const PostsPage = () => {
-  // set up state for your data
+  const [userPosts, setUserPosts] = useState(dummyData);
+
   return (
-    <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
-    </div>
+      <div className="posts-container-wrapper">
+        {userPosts.map((post) => <Post post={post}/>)}
+      </div>
   );
+
 };
 
 export default PostsPage;
